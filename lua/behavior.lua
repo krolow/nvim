4195be42143e33a2156c8308c45d8d99825104f3
Vinicius Krolow <krolow@gmail.com>
2023-03-23 17:13:43 -0300

    More plugins please

--- lua/behavior.lua
+++ lua/behavior.lua
@@ -3,3 +3,6 @@ vim.opt.complete = "kspell"
 vim.cmd("setlocal spell spelllang=en_us")
 vim.opt.backup = false
 vim.opt.writebackup = false
+
+-- Golang
+vim.cmd("au BufWritePost *.go silent! !gofmt -w %")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,57 +1,63 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-		  'nvim-tree/nvim-web-devicons', -- optional, for file icons
-	  }
-  }
-  use {
-	  'akinsho/bufferline.nvim', 
-	  tag = "v3.*", 
-	  requires = 'nvim-tree/nvim-web-devicons',
-  }
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'navarasu/onedark.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'lewis6991/gitsigns.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  -- Hrsh7th Code Completion Suite
-  use 'hrsh7th/nvim-cmp' 
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-vsnip'                             
-  use 'hrsh7th/cmp-path'                              
-  use 'hrsh7th/cmp-buffer'                            
-  use 'hrsh7th/vim-vsnip'
- -- DAP for debugging
- use 'mfussenegger/nvim-dap'
- -- UI for DAP
- use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-  -- Treesitter
- use {
-  -- recommended packer way of installing it is to run this function, copied from documentation
-         'nvim-treesitter/nvim-treesitter',
-      run = function()
-              local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-              ts_update()
-         end,
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		}
+	}
+	use {
+		'akinsho/bufferline.nvim', 
+		tag = "v3.*", 
+		requires = 'nvim-tree/nvim-web-devicons',
+	}
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use 'navarasu/onedark.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'lewis6991/gitsigns.nvim'
+	use 'windwp/nvim-autopairs'
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
+	-- Hrsh7th Code Completion Suite
+	use 'hrsh7th/nvim-cmp' 
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-nvim-lua'
+	use 'hrsh7th/cmp-nvim-lsp-signature-help'
+	use 'hrsh7th/cmp-vsnip'                             
+	use 'hrsh7th/cmp-path'                              
+	use 'hrsh7th/cmp-buffer'                            
+	use 'hrsh7th/vim-vsnip'
+	-- DAP for debugging
+	use 'mfussenegger/nvim-dap'
+	-- UI for DAP
+	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+	-- Treesitter
+	use {
+		-- recommended packer way of installing it is to run this function, copied from documentation
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
 
- }
+	}
+	use 'gpanders/editorconfig.nvim'
+	use {
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+	}
+	use 'ray-x/go.nvim'
 end)
