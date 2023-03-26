79e28a62c1d33945806ede5e0fe7d123f00dd1e4
Vinicius Krolow <krolow@gmail.com>
2023-03-26 11:45:57 -0300

    Command to trouble quickfix

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -2,4 +2,7 @@ require("trouble").setup {
 -- your configuration comes here
 -- or leave it empty to use the default settings
 -- refer to the configuration section below
+	vim.keymap.set("n", "<leader>fix", "<cmd>TroubleToggle quickfix<cr>",
+	  {silent = true, noremap = true}
+	)
 }
